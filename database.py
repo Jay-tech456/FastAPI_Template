45e9c9e2bd0df4ed0fc25c49d56001ca96cba45b
Manjesh Prasad <59748163+Jay-tech456@users.noreply.github.com>
2025-10-03 19:19:29 -0700

    Updated FastAPI Template for interview

--- database.py
+++ database.py
@@ -4,16 +4,17 @@ from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 
+# Creates a SQLAlchemy engine and session --> Connections to Postgres
 load_dotenv()  # load environment variables
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # Gives you a database session per request (like a handle into postgres)
 
-Base = declarative_base()
+Base = declarative_base()  # Special SLQAlchemy class that helps to create models
 
-# Dependency for DB session
+# Safely opens/ closes session per each request
 def get_db():
     db = SessionLocal()
     try:
